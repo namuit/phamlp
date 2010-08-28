fd5979be23758fe7e351a665361e10798167ac75
chris.l.yates@gmail.com <chris.l.yates@gmail.com@e4c289d2-a523-e733-2695-a8574230554f>
2010-08-28 08:31:27 +0000

    Tag the 2.3 release.

---
+++ tags/2.3/haml/HamlException.php
@@ -0,0 +1,16 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Haml exception.
+ * @author			Chris Yates <chris.l.yates@gmail.com>
+ * @copyright 	Copyright (c) 2010 PBM Web Development
+ * @license			http://phamlp.googlecode.com/files/license.txt
+ * @package			PHamlP
+ * @subpackage	Haml
+ */
+/**
+ * Haml exception class.
+ * @package			PHamlP
+ * @subpackage	Haml
+ */
+class HamlException extends Exception {}
---
+++ tags/2.3/sass/SassException.php
@@ -0,0 +1,17 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Sass exception.
+ * @author			Chris Yates <chris.l.yates@gmail.com>
+ * @copyright 	Copyright (c) 2010 PBM Web Development
+ * @license			http://phamlp.googlecode.com/files/license.txt
+ * @package			PHamlP
+ * @subpackage	Sass
+ */
+
+/**
+ * Sass exception class.
+ * @package			PHamlP
+ * @subpackage	Sass
+ */
+class SassException extends Exception {}
